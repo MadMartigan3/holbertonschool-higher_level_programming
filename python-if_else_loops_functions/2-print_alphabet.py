1d45a1304f05906d37a785c7449e3979be8f78b9
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-01-25 15:04:30 +0100

    Fix taks 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for alphabet in range(97, 123):
-    if alphabet == 122:
-        print("{:c}".format(alphabet))
-    else :
-        print("{:c}".format(alphabet), end='')
+    print("{:c}".format(alphabet), end='')
