375538f63bad45b69792b132f4fca1db0538ac46
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-01-30 21:46:28 +0100

    Projet 0 task 6/7/8/9/10/11/12

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for col in range(len(matrix)):
         for row in range(len(matrix[col])):
             print("{:d}".format(matrix[col][row]), end=" ")
-        print()
+        print('')
