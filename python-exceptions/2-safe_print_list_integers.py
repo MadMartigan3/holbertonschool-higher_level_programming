d77ff97e1f262e77967cc8b3be986113ebd94857
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-02 18:50:24 +0100

    projet 2 task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     try:
-        for i in range (x):
+        for i in range(x):
             value = my_list[i]
             if isinstance(value, int):
                 print("{:d}".format(value), end="")
