d431d999663040668bccfe39eb8ae6ff69fe0655
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-02 13:27:04 +0100

    pycodestyle 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return (0)
-    roman_num = {'I': 1, 
+    roman_num = {'I': 1,
                  'V': 5,
                  'X': 10,
                  'L': 50,
