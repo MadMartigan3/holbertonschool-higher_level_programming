c9eb2211df8b81879dcf61908b2c0de4de8d60f9
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-02 13:22:50 +0100

    projet 1 task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return (0)
+    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    prev_value = 0
+
+    for numbers in reversed(roman_string):
+        value = roman_num.get(numbers, 0)
+        if value < prev_value:
+            num -= value
+        else:
+            num += value
+        prev_value = value
+    return num
