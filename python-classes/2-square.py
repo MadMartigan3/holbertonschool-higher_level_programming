e070f675d24301dbd7e210bb4000dde29d35278d
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-05 16:18:12 +0100

    projet 0 task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Create a class Square"""
+
+class Square:
+    def __init__(self, size=0):
+        
+        """The format of parameter is:
+        name (square): description
+        Square is a class type for create a square"""
+
+        """Args:
+        param1 (size): the first parameters"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
