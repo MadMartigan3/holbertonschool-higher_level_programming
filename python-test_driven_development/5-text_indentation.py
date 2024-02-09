8239eb548fd9aede75f624910f46b94fa75bd8eb
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-09 16:08:01 +0100

    projet 2 task 2

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,8 +4,14 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    last_char_special = False
+
     for char in text:
         if char in ['.', '?', ':']:
+            if not last_char_special:
+                print("\n")
             print(char + "\n\n", end="")
+            last_char_special = True
         else:
             print(char, end="")
+            last_char_special = False
