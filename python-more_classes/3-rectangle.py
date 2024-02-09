e4776d5a801bed18c04a51586cf23fe8e80318bc
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-09 16:17:03 +0100

    projet 2 task 3 correction

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return (0)
         return (2 * (self.width + self.height))
-    
+
     def __str__(self):
         """Return string"""
         if self.width == 0 or self.height == 0:
