af4a76eab05fd56caf16b6148ebaa4d14ac67d17
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-09 20:59:42 +0100

    projet 1 task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+
+"""function to divide a matrix"""
+
+
 def say_my_name(first_name, last_name=""):
+    """fonction that print a string
+
+    Args:
+    param1 (first_name): the first parameter
+    param2 (last_name): the scd parameter
+    """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
