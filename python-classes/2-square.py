f64a0ea1555b7f123899c7d7d51ab880b6ee36a8
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-09 10:45:00 +0100

    projet 0 task 2 correction

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -15,8 +15,7 @@ class Square:
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        else:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
