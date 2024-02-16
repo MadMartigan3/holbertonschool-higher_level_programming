5fa713b42d11fae9436a7071fb2d3b81a8a5c12e
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-16 10:19:45 +0100

    projet 1 task 9 pycodestyle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-
+"""Define class rectangle"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """class rectangle inherits from geometry"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-    
+
     def area(self):
+        """Return area of rectangle"""
         return self.__width * self.__height
-    
+
     def __str__(self):
+        """string representation"""
         return (f"[Rectangle] {self.__width}/{self.__height}")
