e59064ce28caf48a0ced8e82d6c0a00cc9c1bb38
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-16 10:16:24 +0100

    projet 1 task 8 pycodestyle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
+"""Define Class Rectangle"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
+    """Class for rectangle inherits from geometry"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
