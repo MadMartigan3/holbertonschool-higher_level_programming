e4829d12b33a1d051421605b4f9b0f154f91e235
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-16 10:23:40 +0100

    projet 1 task 10 pycodestyle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
+"""Define class Square"""
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Class for square inherits from rectangle"""
     def __init__(self, size):
+        """Square instance"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
+        """Return area of square"""
         return (self.__size * self.__size)
