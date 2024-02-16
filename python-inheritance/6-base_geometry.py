bf0bd263c29929cc0b377f0982d7e7bed5cf2c4c
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-16 09:59:42 +0100

    projet 1 task 6 pycodestyle

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Define Class geometry public instance area"""
+
+
 class BaseGeometry:
+    """Class area public instance"""
     def area(self):
         raise Exception("area() is not implemented")
