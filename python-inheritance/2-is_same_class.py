8c567350b0dc0c57d673735bf2b791bde61d6953
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-16 09:46:40 +0100

    projet 1 task 2 pycodestyle

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Define function is_same_class"""
+
+
 def is_same_class(obj, a_class):
+    """Return True if object is an instance of class"""
     if type(obj) is a_class:
         return True
     else:
