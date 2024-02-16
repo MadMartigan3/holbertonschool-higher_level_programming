89c49a76d5cd473dfdac004c09ab47a51db6c196
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-16 09:55:43 +0100

    projet 1 tast 4 pycodestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Define function if object is an instance of class"""
+
+
 def inherits_from(obj, a_class):
+    """Return True if object is an instance of class"""
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
