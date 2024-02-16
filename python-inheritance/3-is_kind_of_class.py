2c1f0ebc211f50c537d6098df1badb98661c0532
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-16 09:52:57 +0100

    projet 1 task 3 pycodestyle

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""Define function is_kind_of_class is instance of class"""
+
+
+
 def is_kind_of_class(obj, a_class):
+    """Return True if object is an instance of the class"""
     return isinstance(obj, a_class)
