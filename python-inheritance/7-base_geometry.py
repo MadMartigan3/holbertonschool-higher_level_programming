bba35a32aa7f401db14826603f921a9895568148
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-18 16:02:05 +0100

    projet 1 task 7 pycodestyle

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""defines class BaseGeomtry
+with public instance methods for area and integer validation"""
+
+
 class BaseGeometry:
+    """class with public instance"""
     def area(self):
+        """Raise exception for area method"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validation integer"""
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
 
