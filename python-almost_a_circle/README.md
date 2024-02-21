f0c711bfcacf0ae3931229ea960a2e9edec997f3
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-02-21 21:26:57 +0100

    projet 1/2

---
+++ python-almost_a_circle/README.md
@@ -0,0 +1,1 @@
+Projet almost a circle
---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+"""Define a class Rectangle from Base"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Private att width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set Private att width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Private att height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set Private att height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Private att X"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set Private att X"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Private att Y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set Private att Y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Return area of rectangle"""
+        return (self.__width * self.__height)
+
+    def display(self):
+        """Print #"""
+        for i in range(self.__y):
+            print("")
+        for j in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        """String str"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """update att"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
