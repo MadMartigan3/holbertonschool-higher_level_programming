f2e37bb0e5a2cf7c38bfae37aef8a9051282e7f3
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-03-19 10:02:02 +0100

    Projet 0 full + corrigé

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+SHOW CREATE TABLE first_table;
